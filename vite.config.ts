66d3dad954022de1879c76d6d85059cacdea1b34
KatMelon <katmelon@foxmail.com>
2024-04-24 18:37:06 +0800

    change base for gh pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/kawaii-dev-logos-website/",
   plugins: [react()],
   resolve: {
     alias: {
